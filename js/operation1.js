db5c3640c514b6228a9489f51ca2e7e059cf9fab
AKASH <akash.meena23@gmail.com>
2022-06-29 15:28:56 +0530

    FirstQ

---
+++ js/operation1.js
@@ -0,0 +1,17 @@
+/****DAY 1 - Operators and Expressions****/
+//Question 1 => Add two Numbers : 
+// You are provided with two numbers
+// A
+// and
+// B
+// Your task is to add these two numbers.
+
+// Note: You have to complete AddTwoNumbers function. No need to take any input.
+// Return the result after adding two numbers.
+
+// let firstNumb = 20 , secondNumb = 30;
+
+const AddtwoNumbers = (firstNumb = 10, secondNumb = 5) => {
+    return (firstNumb + secondNumb);
+}
+console.log(AddtwoNumbers(20 , 30));
